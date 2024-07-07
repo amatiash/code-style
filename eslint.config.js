e5c498dbb7957b138dab31694aabf6b472b93ec0
Anton Matiash <4305962+amatiash@users.noreply.github.com>
2024-07-07 23:00:36 +0200

    chore: Update eslint.config.js with stylistic rules and function-call-spacing

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,28 @@
-import globals from "globals";
-import eslintJs from "@eslint/js";
-import eslintTs from "typescript-eslint";
+import globals from 'globals'
+import eslintJs from '@eslint/js'
+import eslintTs from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  eslintJs.configs.recommended,
-  ...eslintTs.configs.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    eslintJs.configs.recommended,
+    ...eslintTs.configs.recommended,
     stylistic.configs.customize({
-    indent: 4,
-    quotes: 'single',
-    semi: false,
-    jsx: true,
-    arrowParens: true,
-    braceStyle: 'stroustrup',
-    quoteProps: 'as-needed'
+        indent: 4,
+        quotes: 'single',
+        semi: false,
+        jsx: true,
+        arrowParens: true,
+        braceStyle: 'stroustrup',
+        quoteProps: 'as-needed',
     // ...
-  }),
-  // ...you other config items
-];
+    }),
+    {
+        rules: {
+            '@stylistic/function-call-spacing': ['error', 'never'],
+        },
+    },
+    // ...you other config items
+]
